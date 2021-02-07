635845994d16908ef1cce742534a127e8e963afa
timoshishi <timfrrst@gmail.com>
2021-02-07 13:51:58 -0500

    OCR function tweaks

--- functions/ocr/index.js
+++ functions/ocr/index.js
@@ -1,16 +1,16 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const firebase = require('firebase');
-admin.initializeApp(functions.config().firebase);
-// Get a reference to the Cloud Vision API component
-const Vision = require('@google-cloud/vision');
-const visionClient = new Vision.ImageAnnotatorClient();
+// const functions = require('firebase-functions');
+// const admin = require('firebase-admin');
+// const firebase = require('firebase');
+// admin.initializeApp(functions.config().firebase);
+// // Get a reference to the Cloud Vision API component
+// const Vision = require('@google-cloud/vision');
+// const visionClient = new Vision.ImageAnnotatorClient();
 
-const bucketName = 'timoshishi-bughub';
+// const bucketName = 'timoshishi-bughub';
 
-exports.imageTagger = firebase.storage
-  .bucket(bucketName)
-  .object()
-  .onFinalize(async (object) => {
-    console.error({ object });
-  });
+// exports.imageTagger = firebase.storage
+//   .bucket(bucketName)
+//   .object()
+//   .onFinalize(async (object) => {
+//     console.error({ object });
+//   });
