186dbbba62d07fa94f391445fdc3248698a7dc0a
timoshishi <timfrrst@gmail.com>
2021-02-21 15:05:01 -0500

    Remove Counter

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+// import counterReducer from '../features/counter/counterSlice';
 import authReducer from './reducers/authSlice';
 import postReducer from './reducers/postSlice';
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
+    // counter: counterReducer,
     auth: authReducer,
     post: postReducer,
   },
